d51b1c1ba83863c63a0c7aeeb3dfa457c880ac37
Luis Soares <luissoaress.dev@gmail.com>
2022-03-27 22:46:54 -0300

    verificação de inputs okay

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+let senha = document.querySelector(".fa-eye");
+let enviar = document.querySelector(".btn");
+
+senha.addEventListener("click", () => {
+  let password = document.querySelector("#password");
+
+  if (password.getAttribute("type") == "password") {
+    password.setAttribute("type", "text");
+  } else {
+    password.setAttribute("type", "password");
+  }
+});
+
+enviar.addEventListener("click", () => {
+  location.reload();
+});
